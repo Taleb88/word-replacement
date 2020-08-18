b04a11eff9bf2320fea412744e1d58928a049370
Taleb88 <tdamaree18@gmail.com>
2020-08-18 16:45:51 -0400

    Word replacement

---
+++ script.js
@@ -0,0 +1,9 @@
+function chocolate() {
+    let x = document.getElementById('h1');
+    x.outerHTML = '<h1>Taleb actually loves chocolate.</h1>';
+}
+
+function knick() {
+    let x = document.getElementById('h2');
+    x.outerHTML = '<h2>Taleb really loves the Knicks.</h2>'
+}
